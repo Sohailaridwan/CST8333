35e61f5d6f7cd865b45a23edabb4c861a4c9e4b2
Sohailaridwan <ridwan.sohaila@gmail.com>
2018-03-26 04:06:23 +0800

    add test for yearly average

--- tests/test_stats.py
+++ tests/test_stats.py
@@ -1,7 +1,5 @@
-import os
-from datetime import datetime
 from .context import model
 
 def test_yearly_average():
-    print(model.Stats.get_yearly_average(2015, 12))
-    assert type(model.Stats.get_yearly_average(2015, 12)) is type(float), 'Invalid average value!'
+    value = model.Stats.get_yearly_average(2010, 26)
+    assert type(value) is float, 'Invalid type for average value!'
